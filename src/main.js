bf5e2899beda04b5bff74cbf623186ae207d4a23
KaruizawaKei <1569633455@qq.com>
2019-11-03 23:58:44 +0800

    Vue项目搭建

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// import Vue from 'vue'
+// import App from './App.vue'
+import Vue from 'vue/dist/vue.common.js'
+
+Vue.config.productionTip = false
+
+new Vue({
+  data: {
+    msg: 'Misaka丶御坂'
+  }
+  // render: h => h(App)
+}).$mount('#app')
